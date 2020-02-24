dcc97cbdd26f8c4a73ed203960a3fb67dff462bc
Kashif Ullah <kashif.ullah@mobilelive.ca>
2020-02-24 13:52:43 +0500

    import and use new-letter model

--- src/app/news-letter-form/news-letter-form.component.ts
+++ src/app/news-letter-form/news-letter-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsLetter } from '../news-letter';
 
 @Component({
   selector: 'app-news-letter-form',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-letter-form.component.scss']
 })
 export class NewsLetterFormComponent implements OnInit {
+
+  model = new NewsLetter(1, 'Kashif', 'Ullah', 'kashif.ullah@mobilelive.ca');
 
   constructor() { }
 
