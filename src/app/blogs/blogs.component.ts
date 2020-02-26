65baf2e48f8af602083eaee3ae01fcb45153927f
Kashif Ullah <kashif.ullah@mobilelive.ca>
2020-02-26 12:56:00 +0500

    add keydown event

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Blog } from '../blog';
 import { BlogService } from '../blog.service';
 import { SoliderCommunicationService } from '../solider-communication.service';
+import { Observable, fromEvent } from 'rxjs';
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
@@ -18,6 +19,18 @@ export class BlogsComponent implements OnInit {
     this.getBlogs();
     this.soliderCommService.establishCommunication();
     this.soliderCommService.notify();
+
+    // Event Subscription
+    const ESC_KEY = 27;
+    const nameInput = document.getElementById('name') as HTMLInputElement;
+
+    const subscription = fromEvent(nameInput, 'keydown')
+      .subscribe((e: KeyboardEvent) => {
+        console.log(e);
+        if (e.keyCode === ESC_KEY) {
+          nameInput.value = '';
+        }
+      });
   }
 
   onSelect(blog) {
@@ -27,6 +40,20 @@ export class BlogsComponent implements OnInit {
   getBlogs(): void {
     this.blogService.getBlogs().subscribe((response) => {
       this.blogs = response;
+    });
+  }
+
+  fromEvent(target, eventName) {
+    return new Observable((observer) => {
+      const handler = (e) => observer.next(e);
+
+      // Add the event handler to the target
+      target.addEventListener(eventName, handler);
+
+      return () => {
+        // Detach the event handler from the target
+        target.removeEventListener(eventName, handler);
+      };
     });
   }
 
