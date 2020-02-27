43ccffeffcc585ab88bcc2d2592c3509b2c02bff
Kashif Ullah <kashif.ullah@mobilelive.ca>
2020-02-27 09:48:51 +0500

    add subscribe us routing & link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogsComponent } from './blogs/blogs.component';
 import { AddBookComponent } from './add-book/add-book.component';
+import { NewsLetterFormComponent } from './news-letter-form/news-letter-form.component';
 
 
 const routes: Routes = [
   { path: 'listings', component: BlogsComponent },
   { path: 'add-new', component: AddBookComponent },
+  { path: 'subscribe-us', component: NewsLetterFormComponent },
   { path: '', redirectTo: '/listings', pathMatch: 'full' },
 ];
 
