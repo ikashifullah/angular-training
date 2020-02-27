b3c284122f52cb2249410c90ea49bbe4a380543e
Kashif Ullah <kashif.ullah@mobilelive.ca>
2020-02-27 09:44:48 +0500

    add new routing and link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogsComponent } from './blogs/blogs.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 
 const routes: Routes = [
-  { path: 'listings', component: BlogsComponent }
+  { path: 'listings', component: BlogsComponent },
+  { path: 'add-new', component: AddBookComponent }
 ];
 
 @NgModule({
